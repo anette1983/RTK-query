9098dca717804f7d7f30ea85299b2a40cda9f44b
Hanna <anna_kon@bigmir.net>
2023-04-17 19:22:45 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from 'redux/store';
+import { App } from 'components/App/App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = {
+  colors: {
+    white: '#ffffff',
+    black: '#010101',
+    green: '#4caf50',
+    red: '#f44336',
+    blue: '#2196f3',
+    primaryText: '#212121',
+    secondaryText: '#757575',
+  },
+};
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
